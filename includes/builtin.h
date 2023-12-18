228ef2af1fa401f9bba7228568c3f5daeb959845
Luca Morelli <lmorelli@e4r7p3.42roma.it>
2023-12-18 20:16:03 +0100

    exp-unset_18/12_20:15

--- includes/builtin.h
+++ includes/builtin.h
@@ -3,6 +3,7 @@
 
 void	handle_pwd(void);
 void	handle_export(t_general *general, t_lex *node);
+//void	handle_unset(t_general *general, t_lex *node);
 void	handle_unset(t_general *general, t_lex *node);
 void	handle_env(t_general *general);
 int		my_setenv(char *name, char *value, char ***environ);
--- src/builtin/ft_myunset.c
+++ src/builtin/ft_myunset.c
@@ -3,15 +3,83 @@
 /*                                                        :::      ::::::::   */
 /*   ft_myunset.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jcardina <jcardina@student.42roma.it>      +#+  +:+       +#+        */
+/*   By: lmorelli <lmorelli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 14:28:33 by jcardina          #+#    #+#             */
-/*   Updated: 2023/12/15 14:47:40 by jcardina         ###   ########.fr       */
+/*   Updated: 2023/12/18 20:08:23 by lmorelli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/mini_shell.h"
 
+
+/*CONTROLLI ARGOMENTI
+  MESSAGGI DI ERRORE
+  ACCORPARE FUNZIONI SE POSSIBILE
+*/
+
+int ft_unsetEXP(char *name, t_general *general)
+{
+	int i;
+	int j;
+
+	i = 0;
+    while (general->enexp[i])
+	{
+        if (ft_strncmp(general->enexp[i], name, ft_strlen(name)) == 0 && general->enexp[i][ft_strlen(name)] == '=')
+		{
+			j = i;
+            while (general->enexp[j])
+			{
+                general->enexp[j] = general->enexp[j + 1];
+				j++;
+            }
+            return (0);
+        }
+		i++;
+    }
+    return (1);
+}
+
+int ft_unsetenv(char *name, t_general *general)
+{
+	int i;
+	int j;
+
+	i = 0;
+    while (general->envp2[i])
+	{
+        if (ft_strncmp(general->envp2[i], name, ft_strlen(name)) == 0 && general->envp2[i][ft_strlen(name)] == '=')
+		{
+			j = i;
+            while (general->envp2[j])
+			{
+                general->envp2[j] = general->envp2[j + 1];
+				j++;
+            }
+            return (0);
+        }
+		i++;
+    }
+    return (1);
+}
+
+
+void	handle_unset(t_general *general, t_lex *node)
+{
+	//aggiungere controlli argomenti
+
+	if (ft_unsetenv(node->command2[1], general) == 0)
+		printf("UnsetENV: %s\n", node->command2[1]);
+	else
+		printf("error\n");			//sistemare messaggi di errore
+	if (ft_unsetEXP(node->command2[1], general) == 0)
+		printf("UnsetEXP: %s\n", node->command2[1]);
+	else
+		printf("error\n");
+}
+
+/*
 void	handle_unset(t_general *general, t_lex *node)
 {
 	int		i;
@@ -35,3 +103,4 @@ void	handle_unset(t_general *general, t_lex *node)
 	free_matrix(general->envp2);
 	general->envp2 = envtmp;
 }
+*/
