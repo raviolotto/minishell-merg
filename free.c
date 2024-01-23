f719def387052fdd88d8a1b526f5fb27f714803a
ani mema <amema@anis-MacBook-Pro.local>
2024-01-23 18:18:37 +0100

    spero sia il mio branch2

--- free.c
+++ free.c
@@ -6,22 +6,32 @@
 /*   By: amema <amema@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 19:33:03 by amema             #+#    #+#             */
-/*   Updated: 2024/01/19 16:33:18 by amema            ###   ########.fr       */
+/*   Updated: 2024/01/23 18:15:40 by amema            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/mini_shell.h"
 
-// void	free_matrix(char ***matrix)
-// {
-// 	int	j;
+void	free_matrix(char **matrix)
+{
+	size_t	i;
 
-// 	j = 0;
-// 	while ( j < ++)
-// 		free(matrix[i]);
-// 	free(matrix);
-// }
-
+	i = 0;
+	while (matrix[i] != NULL)
+	{
+		free (matrix[i]);
+		i++;
+	}
+	free (matrix);
+}
+void	afalcons(t_lex *node)
+{
+	if (node->next)
+		afalcons(node->next);
+	free_matrix(node->command2);
+	free(node->command);
+	free(node);
+}
 
 
 // void	free_matrix1(char **matrix)
@@ -48,8 +58,9 @@ void free_lex(t_lex *node)
 {
 	if (node == NULL)
 		return;
-	free_matrix(&(node->command2)); // +altra roba che devo freeare(?)
+	free_matrix(&(node->command2)); // +altra roba che devo freeare(?) ---->decidere se freeare con questo oppure afalcons
 	free_lex(node->next);
+	free_matrix(&(node->command)); // +altra roba che devo freeare(?)
 	free(node);
 }
 
