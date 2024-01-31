a4b4172fbf30010d1fb55fd382701a3cf6583234
Jacopo Cardinaux <jcardina@e4r12p2.42roma.it>
2024-01-31 14:03:31 +0100

    ripristino mini_shell.c

--- src/mini_shell.c
+++ src/mini_shell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mini_shell.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frdal-sa <frdal-sa@student.42roma.it>      +#+  +:+       +#+        */
+/*   By: jcardina <jcardina@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/30 19:50:35 by frdal-sa          #+#    #+#             */
-/*   Updated: 2024/01/30 19:50:35 by frdal-sa         ###   ########.fr       */
+/*   Created: 2023/11/07 16:17:29 by jcardina          #+#    #+#             */
+/*   Updated: 2024/01/31 13:54:41 by jcardina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,38 +32,21 @@ void	init(t_general *general, char **envp)
 	general->envp2 = matrix_dup(envp);
 	general->enexp = matrix_dup(general->envp2);
 	general->path = NULL;
+	//general->path = ft_split(getenv("PATH"), ':');
 	printf(PINK"\n%s\n", INTRO);
 	printf("\n%s\n"RESET, HELLO2);
-}
-
-int	check_input_number(int ac)
-{
-	if (ac != 1)
-	{
-		printf(RED "error dumb input\n" RESET);
-		return (0);
-	}
-	return (1);
-}
-
-void	start_the_work(t_general general)
-{
-	t_lex		*tmp;
-
-	parser(&general);
-	tmp = general.lexer;
-	expander(&general);
-	executor(&general);
-	free_lex(general.lexer);
-	general.lexer = NULL;
 }
 
 int	main(int ac, char **av, char **envp)
 {
 	t_general	general;
+	t_lex		*tmp;
 
-	if (!check_input_number(ac))
+	if (ac != 1)
+	{
+		printf(RED "error dumb input\n" RESET);
 		return (0);
+	}
 	init(&general, envp);
 	while (1)
 	{
@@ -72,11 +55,34 @@ int	main(int ac, char **av, char **envp)
 		if (general.args == NULL)
 		{
 			ft_printf("sono uscito nel controllo general.args == NULL\n");
-			free_and_exit(1, &general);
+			free_and_exit(0, &general);
 		}
 		if (general.args && *general.args)
 			add_history(general.args);
 		if (!is_whitespace_input(general.args))
-			start_the_work(general);
+		{
+			parser(&general);
+
+			//                       debug info
+			tmp = general.lexer;
+			// printf(RED);
+			// while (tmp != NULL)
+			// {
+			// 	printf("token %i\n", tmp->token);
+			// 	printf("pipe steatus %i\n", tmp->pipe_status);
+			// 	printf("command %s\n", tmp->command);
+			// 	printf("builtin == %i\n", tmp->builtin);
+			// 	print_matrix(tmp->command2);
+			// 	printf("\n");
+			// 	tmp = tmp->next;
+			// }
+			// printf(RESET);
+			//                       end debug info;
+
+			expander(&general);
+			executor(&general);
+			free_lex(general.lexer);
+			general.lexer = NULL;
+		}
 	}
 }
